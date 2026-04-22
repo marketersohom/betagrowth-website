e63c944df7d041e72c526b89fa56c141999ee7da
Sohom Mukherjee <sohommukh1@gmail.com>
2026-04-22 21:31:13 +0700

    fix: add cdn.sanity.io to images.domains for Sanity image rendering

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   },
   // Required for Sanity Studio embedded in Next.js App Router
   transpilePackages: ["@sanity/ui", "@sanity/icons", "styled-components"],
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
 };
 
 export default nextConfig;
